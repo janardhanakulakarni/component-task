4eb9cf26bdb13fdbd64fba0d943d9e049ab4e1e1
pavan <hpavanb1998@gmail.com>
2021-02-26 17:51:04 +0530

    vuefirstpro

---
+++ vuejs/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            title: 'The-10 startups in india'
+        }
+    }
+}
+</script>
+<style scoped>
+header{
+    background: lightgreen;
+    padding: 10px;
+}
+h1{
+    color: rgb(197, 34, 34);
+    text-align: center;
+}
+</style>
